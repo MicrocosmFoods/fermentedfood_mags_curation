66b8779772d169530bf6cea5044969cfd8ab0d9b
Elizabeth McDaniel <elizabethmcd93@gmail.com>
2024-10-30 17:27:30 -0700

    updated code for organizing metadata substrate categories, wip

--- scripts/food-metagenome-mag-metadata-org.R
+++ scripts/food-metagenome-mag-metadata-org.R
@@ -5,9 +5,11 @@ library(tidyverse)
 # Du 2023 BGC MAGs
 du2023_metadata <- read.csv("metadata/raw_metadata/mag_datasets/FermentedFoodBGCs/Du2023_MAG_metadata.csv") %>% 
   select(MAG_id, Food.fermentation, Genome.size..bp., Completeness...., Contamination...., Domain, Phylum, Class, Order, Family, Genus, Species) %>% 
-  mutate(study_catalog = c("Du2023"))
+  mutate(study_catalog = c("Du2023")) %>% 
+  mutate(substrate_category = Food.fermentation) %>% 
+  mutate(source = NA)
 
-colnames(du2023_metadata) <- c("mag_id", "substrate", "genome_size", "completeness", "contamination", "domain", "phylum", "class", "order", "family", "genus", "species", "study_catalog")
+colnames(du2023_metadata) <- c("mag_id", "specific_substrate", "genome_size", "completeness", "contamination", "domain", "phylum", "class", "order", "family", "genus", "species", "study_catalog", "substrate_category", "source")
 
 du2023_metadata_cleaned <- du2023_metadata %>% 
   mutate(domain = gsub("d__", "", domain)) %>% 
@@ -17,7 +19,8 @@ du2023_metadata_cleaned <- du2023_metadata %>%
   mutate(family = gsub("f__", "", family)) %>% 
   mutate(genus = gsub("g__", "", genus)) %>% 
   mutate(species = gsub("s__", "", species)) %>% 
-  select(-genome_size)
+  select(-genome_size) %>% 
+  select(mag_id, substrate_category, specific_substrate, source, everything())
 
 # Carlino 2024 food metagenomes
 carlino2024_mag_metadata <- read.csv("metadata/raw_metadata/mag_datasets/CellFoodMetagenomics/2024-cell-food-metagenomics-mag-metadata.csv") %>% 
@@ -31,19 +34,23 @@ colnames(carlino2024_sample_metadata) <- c("dataset_sample_id", "substrate", "sp
 
 carlino2024_complete_metadata <- left_join(carlino2024_mag_metadata, carlino2024_sample_metadata) %>% 
   mutate(source = dataset_sample_id) %>% 
-  select(mag_id, substrate, source, completeness, contamination, domain, phylum, class, order, family, genus, species, study_catalog)
+  mutate(substrate_category = substrate) %>% 
+  select(mag_id, substrate_category, specific_substrate, source, completeness, contamination, domain, phylum, class, order, family, genus, species, study_catalog)
 
 # Caffrey 2024 MiFoDB
 caffrey2024_metadata <- read.csv("metadata/raw_metadata/mag_datasets/MiFoDB/MiFoDB_beta_v3_AllReferences.csv") %>% 
-  select(genome, substrate, source, completeness, contamination, domain, phylum, class, order, family, genus, species) %>% 
+  select(genome, description, substrate, source, completeness, contamination, domain, phylum, class, order, family, genus, species) %>% 
   drop_na() %>% 
   mutate(mag_id = genome) %>% 
-  select(-genome) %>% 
+  mutate(specific_substrate = description) %>% 
+  mutate(substrate_category = substrate) %>% 
+  select(-genome, -description, -substrate) %>% 
+  select(mag_id, substrate_category, specific_substrate, source, everything()) %>% 
   mutate(study_catalog = c("Caffrey2024"))
 
 # Saak 2023 cheese MAGs
 saak2023_metadata <- read.csv("metadata/raw_metadata/mag_datasets/Saak2023CheeseMAGs/saak2023_cheese_mags_isolates_metadata.csv") %>% 
-  select(Bin.Name, Completeness, Contamination, gtdbtk.Classifaction) %>% 
+  select(Cheese, Bin.Name, Completeness, Contamination, gtdbtk.Classifaction) %>% 
   drop_na()
 
 saak2023_metadata_cleaned <- saak2023_metadata %>% 
@@ -54,20 +61,23 @@ saak2023_metadata_cleaned <- saak2023_metadata %>%
   mutate(mag_id = Bin.Name) %>% 
   mutate(completeness = Completeness) %>% 
   mutate(contamination = Contamination) %>% 
-  mutate(substrate = c("dairy")) %>% 
+  mutate(specific_substrate = c("cheese")) %>% 
+  mutate(substrate_category = c("cheese")) %>% 
+  mutate(source = paste("cheese_", Cheese)) %>% 
   mutate(study_catalog = c("Saak2023_CheeseMAGs")) %>% 
-  select(mag_id, substrate, completeness, contamination, domain, phylum, class, order, family, genus, species, study_catalog)
+  select(mag_id, substrate_category, specific_substrate, source, completeness, contamination, domain, phylum, class, order, family, genus, species, study_catalog)
 
 # Msystems Sourdough AABs
 sourdough_aabs_metadata <- read.csv("metadata/raw_metadata/mag_datasets/msystemsAAB/SourdoughAAB-MAGs-metadata-modified.csv") %>% 
   filter(Source_environment == "sourdough") %>% 
-  select(assembly_name, Species, Source_category, Completeness, Contam)
+  select(assembly_name, Species, Source_category, Completeness, Contam, Source)
 
-colnames(sourdough_aabs_metadata) <- c("mag_id", "species", "source_category", "completeness", "contamination")
+colnames(sourdough_aabs_metadata) <- c("mag_id", "species", "source_category", "completeness", "contamination", "source")
 
 # taxonomy information from GTDB for Acetobacter spp.
-sourdough_aabs_metadata_cleaned <- sourdough_aabs_metadata %>% 
-  mutate(substrate = c("grains")) %>% 
+sourdough_aabs_metadata_cleaned <- sourdough_aabs_metadata %>%
+  mutate(substrate_category = c("grains")) %>% 
+  mutate(specific_substrate = c("sourdough")) %>% 
   mutate(domain = c("Bacteria")) %>% 
   mutate(phylum = c("Pseudomonadota")) %>% 
   mutate(class = c("Alphaproteobacteria")) %>% 
@@ -76,40 +86,25 @@ sourdough_aabs_metadata_cleaned <- sourdough_aabs_metadata %>%
   select(-source_category) %>% 
   mutate(genus = sub(" .*", "", species)) %>% 
   mutate(study_catalog = c("Rappaport2024_sourdough")) %>% 
-  select(mag_id, substrate, completeness, contamination, domain, phylum, class, order, family, genus, species, study_catalog)
+  mutate(source = c("Rappaport2024_sourdough")) %>% 
+  select(mag_id, substrate_category, specific_substrate, source, completeness, contamination, domain, phylum, class, order, family, genus, species, study_catalog)
   
 
 # combine all dataset metadata
-caffrey_carlino_dbs <- bind_rows(carlino2024_complete_metadata, caffrey2024_metadata) %>% 
+caffrey_carlino_dbs <- bind_rows(carlino2024_complete_metadata, caffrey2024_metadata) %>%
   filter(contamination < 10)
 
 write.csv(caffrey_carlino_dbs, "metadata/cleaned_metadata/mag_datasets/caffrey_carlino_datasets.csv", quote = FALSE, row.names = FALSE)
 
 all_food_mags_metadata <- bind_rows(du2023_metadata_cleaned, carlino2024_complete_metadata, caffrey2024_metadata, saak2023_metadata_cleaned, sourdough_aabs_metadata_cleaned) %>% 
-  select(-source) %>% 
   filter(contamination < 10) %>% 
-  mutate(substrate = tolower(substrate))
-
-all_food_mags_metadata %>% 
-  ggplot(aes(x=contamination, y=completeness)) +
-  geom_point(aes(color=phylum))
-
-all_food_mags_metadata %>% 
-  group_by(substrate) %>% 
-  count() %>% 
-  arrange(desc(n)) %>% 
-  print(n=40)
-
-all_food_mags_metadata %>% 
-  group_by(substrate) %>% 
-  filter(study_catalog == "Caffrey2024") %>% 
-  count() %>% 
-  arrange(desc(n)) %>% 
-  select(-n)
+  mutate(substrate_category = tolower(substrate_category)) %>% 
+  mutate(specific_substrate = tolower(specific_substrate))
 
 # substrate categories reorganized to match broad categories in Caffrey 2024
 # below the first word listed is the broad category, after the arrow are entries that were changed to the broad category. 
-# dairy -> cheese, milk kefir, nunu, koumiss
+# phyla are simplified into "groups" based on previous NCBI major groupings
+# dairy -> cheese, nunu, koumiss
 # grains  -> fermented_grains, sourdough
 # salt     
 # soy -> soy sauce
@@ -118,51 +113,77 @@ all_food_mags_metadata %>%
 # seafood  -> fermented_fish, fish
 # meat  -> fermented_meat  
 # seed -> fermented_seeds
-# However fruit and vegetable separate cateogries were combined as the Carlino2024 study has these grouped together. Below are the new custom categories added on top of Caffrey2024, or merged into categories from other studies
+# However fruit and vegetable separate categories were combined as the Carlino2024 study has these grouped together. Below are the new custom categories added on top of Caffrey2024, or merged into categories from other studies
 # fruits_and_vegetables -> fruit, vegetables, fermented_fruits_and_vegetables, kimchi
 # fermented_beverages -> alcohol, chinese liquor, wine, kombucha
 # \\<pattern\\>
 
-all_food_mags_metadata_cleaned <- all_food_mags_metadata %>% 
-  mutate(substrate = gsub("cheese", "dairy", substrate)) %>% 
-  mutate(substrate = gsub("dairy ", "dairy", substrate)) %>% 
-  mutate(substrate = gsub("fermented_fruits_and_vegetables", "fruits_and_vegetables", substrate)) %>% 
-  mutate(substrate = gsub("fermented_meat", "meat", substrate)) %>% 
-  mutate(substrate = gsub("fermented_grains", "grains", substrate)) %>% 
-  mutate(substrate = gsub("fermented_legumes", "legumes", substrate)) %>% 
-  mutate(substrate = gsub("fermented_seeds", "seed", substrate)) %>% 
-  mutate(substrate = gsub("\\<fruit\\>", "fruits_and_vegetables", substrate)) %>% 
-  mutate(substrate = gsub("\\<vegetable\\>", "fruits_and_vegetables", substrate)) %>% 
-  mutate(substrate = gsub("fermented_fish", "seafood", substrate)) %>% 
-  mutate(substrate = gsub("milk kefir", "dairy", substrate)) %>% 
-  mutate(substrate = gsub("chinese liquor", "fermented_beverages", substrate)) %>% 
-  mutate(substrate = gsub("wine", "fermented_beverages", substrate)) %>% 
-  mutate(substrate = gsub("fish", "seafood", substrate)) %>% 
-  mutate(substrate = gsub("sourdough", "grains", substrate)) %>% 
-  mutate(substrate = gsub("soy sauce", "soy", substrate)) %>% 
-  mutate(substrate = gsub("nunu", "dairy", substrate)) %>% 
-  mutate(substrate = gsub("kimchi", "fruits_and_vegetables", substrate)) %>% 
-  mutate(substrate = gsub("kombucha", "fermented_beverages", substrate)) %>% 
-  mutate(substrate = gsub("alcohol", "fermented_beverages", substrate)) %>% 
-  mutate(substrate = gsub("koumiss", "dairy", substrate))
-
-all_food_mags_metadata_cleaned <- all_food_mags_metadata_cleaned %>% 
-  mutate(group = case_when(
-    phylum == "Pseudomonadota" ~ "Proteobacteria",
-    str_detect(phylum, "Firmicutes") ~ "Firmicutes",
-    phylum == "Actinobacteriota" ~ "Actinobacteria",
-    phylum == "Actinomycetota" ~ "Actinobacteria",
-    phylum == "Bacteroidota" ~ "Bacteroidetes",
-    phylum == "Bacillota" ~ "Firmicutes",
-    phylum == "Halobacteriota" ~ "Euryarchaea",
-    phylum == "Acidobacteriota" ~ "Acidobacteria", 
-    phylum == "Armatimonadota" ~ "Armatimonadetes",
-    phylum == "Deinococcota" ~ "Deinococcus_Thermus",
-    phylum == "Thermoproteota" ~ "Euryarchaea",
-    phylum == "Euryarchaeota" ~ "Euryarchaea",
-    phylum == "Balneolaeota" ~ "Bacteroidetes",
-    TRUE ~ phylum  # Keeps the original phylum name if no rule matches
-  ))
+all_food_mags_metadata_cleaned <- all_food_mags_metadata %>%
+  mutate(
+    # Trim whitespace
+    original_substrate = specific_substrate,
+    specific_substrate = trimws(specific_substrate, which = "both"),
+    
+    # Assign specific_substrate based on keywords in original_substrate
+    specific_substrate = case_when(
+      grepl("cheese", specific_substrate, ignore.case = TRUE) ~ "cheese",
+      grepl("kefir", specific_substrate, ignore.case = TRUE) ~ "kefir",
+      grepl("yoghurt|yogurt", specific_substrate, ignore.case = TRUE) ~ "yogurt",
+      grepl("kombucha", specific_substrate, ignore.case = TRUE) ~ "kombucha",
+      grepl("rice", specific_substrate, ignore.case = TRUE) ~ "rice",
+      grepl("beer", specific_substrate, ignore.case = TRUE) ~ "beer",
+      grepl("sauerkraut", specific_substrate, ignore.case = TRUE) ~ "sauerkraut",
+      grepl("wine", specific_substrate, ignore.case = TRUE) ~ "wine",
+      grepl("sourdough", specific_substrate, ignore.case = TRUE) ~ "sourdough",
+      grepl("sausage", specific_substrate, ignore.case = TRUE) ~ "sausage",
+      grepl("kimchi", specific_substrate, ignore.case = TRUE) ~ "kimchi",
+      grepl("cocoa", specific_substrate, ignore.case = TRUE) ~ "cocoa",
+      grepl("maize", specific_substrate, ignore.case = TRUE) ~ "maize",
+      TRUE ~ specific_substrate  # Default to the original specific_substrate
+    ),
+    
+    # Apply replacements to categorize substrate
+    substrate_category = substrate_category %>%
+      gsub("cheese", "dairy", .) %>%
+      gsub("dairy ", "dairy", .) %>%
+      gsub("fermented_fruits_and_vegetables", "fruits_and_vegetables", .) %>%
+      gsub("fermented_meat", "meat", .) %>%
+      gsub("fermented_grains", "grains", .) %>%
+      gsub("fermented_legumes", "legumes", .) %>%
+      gsub("fermented_seeds", "seed", .) %>%
+      gsub("\\<fruit\\>", "fruits_and_vegetables", .) %>%
+      gsub("\\<vegetable\\>", "fruits_and_vegetables", .) %>%
+      gsub("fermented_fish", "seafood", .) %>%
+      gsub("chinese liquor", "fermented_beverages", .) %>%
+      gsub("wine", "fermented_beverages", .) %>%
+      gsub("fish", "seafood", .) %>%
+      gsub("sourdough", "grains", .) %>%
+      gsub("soy sauce", "soy", .) %>%
+      gsub("nunu", "dairy", .) %>%
+      gsub("kimchi", "fruits_and_vegetables", .) %>%
+      gsub("kombucha", "fermented_beverages", .) %>%
+      gsub("alcohol", "fermented_beverages", .) %>%
+      gsub("koumiss", "dairy", .),
+    
+    # Assign group based on phylum
+    group = case_when(
+      phylum == "Pseudomonadota" ~ "Proteobacteria",
+      str_detect(phylum, "Firmicutes") ~ "Firmicutes",
+      phylum == "Actinobacteriota" ~ "Actinobacteria",
+      phylum == "Actinomycetota" ~ "Actinobacteria",
+      phylum == "Bacteroidota" ~ "Bacteroidetes",
+      phylum == "Bacillota" ~ "Firmicutes",
+      phylum == "Halobacteriota" ~ "Euryarchaea",
+      phylum == "Acidobacteriota" ~ "Acidobacteria",
+      phylum == "Armatimonadota" ~ "Armatimonadetes",
+      phylum == "Deinococcota" ~ "Deinococcus_Thermus",
+      phylum == "Thermoproteota" ~ "Euryarchaea",
+      phylum == "Euryarchaeota" ~ "Euryarchaea",
+      phylum == "Balneolaeota" ~ "Bacteroidetes",
+      TRUE ~ phylum  # Default to original phylum if no match
+    ) 
+  ) %>% 
+  select(mag_id, original_substrate, substrate_category, everything())
 
 all_food_mags_metadata_cleaned %>% 
   group_by(group) %>% 
